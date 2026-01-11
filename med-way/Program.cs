40a05ac7ba05413ecb767fba097aa2bdb6f668dd
Нікіта Ковальчук <Nikita.Kovalchuk@systemgroup.com.ua>
2026-01-11 23:46:00 +0200

    add-swager-ui-when-not-debug

--- med-way/Program.cs
+++ med-way/Program.cs
@@ -21,11 +21,14 @@ namespace med_way
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
